2a8cdc90b9ac7c4e76525ef14b3aba0dede6949b
Bret Fisher <bret@bretfisher.com>
2026-04-13 22:34:23 -0400

    fixing lints (#97)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,16 @@
 set -e
 
 if [ ! -f Gemfile ]; then
-  echo "NOTE: hmm, I don't see a Gemfile so I don't think there's a jekyll site here"
-  echo "Either you didn't mount a volume, or you mounted it incorrectly."
-  echo "Be sure you're in your jekyll site root and use something like this to launch"
-  echo ""
-  echo "docker run -p 4000:4000 -v \$(pwd):/site bretfisher/jekyll-serve"
-  echo ""
-  echo "NOTE: To create a new site, you can use the sister image bretfisher/jekyll like:"
-  echo ""
-  echo "docker run -v \$(pwd):/site bretfisher/jekyll new ."
-  exit 1
+ echo "NOTE: hmm, I don't see a Gemfile so I don't think there's a jekyll site here"
+ echo "Either you didn't mount a volume, or you mounted it incorrectly."
+ echo "Be sure you're in your jekyll site root and use something like this to launch"
+ echo ""
+ echo "docker run -p 4000:4000 -v \$(pwd):/site bretfisher/jekyll-serve"
+ echo ""
+ echo "NOTE: To create a new site, you can use the sister image bretfisher/jekyll like:"
+ echo ""
+ echo "docker run -v \$(pwd):/site bretfisher/jekyll new ."
+ exit 1
 fi
 
 bundle install --retry 5 --jobs 20
